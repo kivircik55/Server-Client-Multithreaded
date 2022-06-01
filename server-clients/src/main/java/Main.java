b599618054b4fc50ff33edb57bafc97d33b96ba7
Oguzhan Kilic <45488599+kivircik55@users.noreply.github.com>
2022-06-01 14:41:05 +0200

    Update Main.java

--- server-clients/src/main/java/Main.java
+++ server-clients/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import client.Client;
 import server.Server;
 
 import java.util.Scanner;
@@ -7,6 +8,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int serverPortNumber=1234;
+        int numberOfClient=0;
 
         System.out.println("Please enter the number of server you would like to launch : ");
         int numberOfServers=Integer.parseInt(scanner.nextLine());
@@ -20,9 +22,13 @@ public class Main {
             serverPortNumber+=count;
             Server.main(new String[]{Integer.toString(serverPortNumber)});
             System.out.println("Server #"+count+1+" has been created at port : "+serverPortNumber);
+
+            System.out.println("Please enter the number of client that you want to connect to this server : ");
+            numberOfClient = Integer.parseInt(scanner.nextLine());
+
+            for (int countClient = 0; countClient < numberOfClient; countClient++){
+                Client.main(new String[]{Integer.toString(serverPortNumber)});
+            }
         }
-
-
-
     }
 }
